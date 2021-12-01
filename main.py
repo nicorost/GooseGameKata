db9584e6db11d62e24c105ac48634f2d65e9d53d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-12-01 09:03:50 +0100

    simplified main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from gui import Gui
 from game import Game
    
-player1_name = 'Julia'   
-player2_name = 'Joe'
-# player1_name = input("Player 1 Name: ")
-# print(f"Player 1: {player1_name}")
-# player2_name = input("Player 2 Name: ")
-# print(f"Player 2: {player2_name}")
 
-game = Game(player1=player1_name, player2=player2_name)
+game = Game(player1='Julia', player2='Joe')
 gui = Gui(game=game)
 gui.run()
